9192c606a4000a54aa0781b9b3bfdbc2d107c577
MsNahid <m.nahidzaman@gmail.com>
2022-05-21 02:03:38 +0600

    made sessionfactory signleton

---
+++ HibernateCrudAndMapping/src/main/java/org/example/utils/HibernateUtils.java
@@ -0,0 +1,28 @@
+package org.example.utils;
+
+import org.example.entities.Student;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+	
+	private static SessionFactory sessionFactory = null;
+	
+	public static SessionFactory getSessionFactory(){
+		if(sessionFactory == null){
+			
+			try{
+				sessionFactory = new Configuration().configure()
+						.addAnnotatedClass(Student.class)
+						.buildSessionFactory();
+				
+			}catch (Exception e){
+				e.printStackTrace();
+				System.out.println("Session Factory not created succesfully!!");
+				
+			}
+		}
+		
+		return sessionFactory;
+	}
+}
